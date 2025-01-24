512779ddb3fdd5d54918f7eb588eb1c45422bf67
Evgen Larkin <l4p41@yandex.com>
2025-01-24 19:00:16 +0300

    added selenoid

---
+++ configure/conf.py
@@ -0,0 +1,29 @@
+import pytest
+from selene import Browser, Config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+# from selene.support.shared import browser
+
+
+@pytest.fixture(scope='function', autouse=True)
+def browser_manager():
+
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "126.0",
+        "selenoid:options": {
+            "enableVideo": True,
+            "enableVNC": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser = Browser(Config(driver))
+
+    yield browser
+
+    browser.quit()
