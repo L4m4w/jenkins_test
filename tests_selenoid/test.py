f5bd4c7b0dbfd83e6394434e28bd49ac69fb5dde
Evgen Larkin <l4p41@yandex.com>
2025-01-24 19:03:46 +0300

    added selenoid

---
+++ tests_selenoid/test.py
@@ -0,0 +1,57 @@
+import allure
+from selene import browser, by, be
+from selene.support.shared.jquery_style import s
+from configure.conf import browser_manager
+
+
+def test_dynamic_steps():
+    with allure.step('Open Git main page'):
+        browser.open('/')
+
+    with allure.step('Search for repo'):
+        s(".header-search-button").click()
+        s(".FormControl-input").send_keys('eroshenkoam/allure-example')
+        s(".FormControl-input").submit()
+
+    with allure.step('Redirect to repo'):
+        s(by.link_text("eroshenkoam/allure-example")).click()
+
+    with allure.step('Open the issues tab'):
+        s("#issues-tab").click()
+
+    with allure.step('Check issue number'):
+        s(by.partial_text("#95")).should(be.visible)
+
+def test_decorator_steps():
+    open_main_page()
+    search_for_repository("eroshenkoam/allure-example")
+    go_to_repository("eroshenkoam/allure-example")
+    open_issue_tab()
+    should_see_issue_with_number("#95")
+
+
+@allure.step("Открываем главную страницу")
+def open_main_page():
+    browser.open("https://github.com")
+
+
+@allure.step("Ищем репозитория {repo}")
+def search_for_repository(repo):
+    s(".header-search-button").click()
+    s(".FormControl-input").send_keys(repo)
+    s(".FormControl-input").submit()
+
+
+@allure.step("Переходим по ссылке репозитория {repo}")
+def go_to_repository(repo):
+    s(by.link_text(repo)).click()
+
+
+@allure.step("Открываем таб Issues")
+def open_issue_tab():
+    s("#issues-tab").click()
+
+
+@allure.step("Проверяем наличие Issue с номером {number}")
+def should_see_issue_with_number(number):
+    s(by.partial_text(number)).click()
